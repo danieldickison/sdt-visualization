cbfaef255125f16d33cf4e8c1d4621e3830312b5
Daniel Dickison <ddickison@daniel.dev>
2014-07-01 13:06:16 -0400

    Better normal cdf function adapted from R's pnorm implementation.

--- js/sdt.js
+++ js/sdt.js
@@ -338,6 +338,7 @@
         return 1.0 / Math.sqrt(2 * Math.PI) * Math.exp(-Math.pow(x,2) / 2.0);
     }
 
+    /*
     // https://en.wikipedia.org/wiki/Normal_distribution#Generating_values_from_normal_distribution
     // Approximation from Zelen & Severo.
     var cdf = (function () {
@@ -348,6 +349,157 @@
             return 1.0 - pdf(x) * (b1*t + b2*p(t,2) + b3*p(t,3) + b4*p(t,4) + b5*p(t,5));
         };
     })();
+    */
+
+    // Better cdf adapted from R's pnorm function.
+    var cdf = (function () {
+        function trunc(x) {
+            return x < 0 ? Math.ceil(x) : Math.floor(x);
+        }
+
+        var SIXTEN = 16, /* Cutoff allowing exact "*" and "/" */
+            DBL_EPSILON = 2.2204460492503131e-16,
+            M_SQRT_32 = Math.sqrt(32),
+            M_1_SQRT_2PI = 1 / Math.sqrt(2 * Math.PI),
+            a = [
+                2.2352520354606839287,
+                161.02823106855587881,
+                1067.6894854603709582,
+                18154.981253343561249,
+                0.065682337918207449113
+            ],
+            b = [
+                47.20258190468824187,
+                976.09855173777669322,
+                10260.932208618978205,
+                45507.789335026729956
+            ],
+            c = [
+                0.39894151208813466764,
+                8.8831497943883759412,
+                93.506656132177855979,
+                597.27027639480026226,
+                2494.5375852903726711,
+                6848.1904505362823326,
+                11602.651437647350124,
+                9842.7148383839780218,
+                1.0765576773720192317e-8
+            ],
+            d = [
+                22.266688044328115691,
+                235.38790178262499861,
+                1519.377599407554805,
+                6485.558298266760755,
+                18615.571640885098091,
+                34900.952721145977266,
+                38912.003286093271411,
+                19685.429676859990727
+            ],
+            p = [
+                0.21589853405795699,
+                0.1274011611602473639,
+                0.022235277870649807,
+                0.001421619193227893466,
+                2.9112874951168792e-5,
+                0.02307344176494017303
+            ],
+            q = [
+                1.28426009614491121,
+                0.468238212480865118,
+                0.0659881378689285515,
+                0.00378239633202758244,
+                7.29751555083966205e-5
+            ];
+        return function cdf_r_pnorm(x) {
+            var cum, ccum;
+            var xden, xnum, temp, del, eps, xsq, y, i;
+
+            /* Consider changing these : */
+            eps = DBL_EPSILON * 0.5;
+            
+            y = Math.abs(x);
+
+            function do_del(X) {
+                xsq = trunc(X * SIXTEN) / SIXTEN;
+                del = (X - xsq) * (X + xsq);
+                cum = Math.exp(-xsq * xsq * 0.5) * Math.exp(-del * 0.5) * temp;
+                ccum = 1.0 - cum;
+            }
+
+            function swap_tail() {
+                if (x > 0) {/* swap  ccum <--> cum */
+                    temp = cum;
+                    cum = ccum;
+                    ccum = temp;
+                }
+            }
+
+            if (y <= 0.67448975) { /* qnorm(3/4) = .6744.... -- earlier had 0.66291 */
+                if (y > eps) {
+                    xsq = x * x;
+                    xnum = a[4] * xsq;
+                    xden = xsq;
+                    for (i = 0; i < 3; ++i) {
+                        xnum = (xnum + a[i]) * xsq;
+                        xden = (xden + b[i]) * xsq;
+                    }
+                } else xnum = xden = 0.0;
+
+                temp = x * (xnum + a[3]) / (xden + b[3]);
+                cum = 0.5 + temp;
+            }
+            else if (y <= M_SQRT_32) {
+                /* Evaluate pnorm for 0.674.. = qnorm(3/4) < |x| <= sqrt(32) ~= 5.657 */
+                xnum = c[8] * y;
+                xden = y;
+                for (i = 0; i < 7; ++i) {
+                    xnum = (xnum + c[i]) * y;
+                    xden = (xden + d[i]) * y;
+                }
+                temp = (xnum + c[7]) / (xden + d[7]);
+
+                do_del(y);
+                swap_tail();
+            }
+
+            /* else   |x| > sqrt(32) = 5.657 :
+             * the next two case differentiations were really for lower=T, log=F
+             * Particularly  *not*  for  log_p !
+
+             * Cody had (-37.5193 < x  &&  x < 8.2924) ; R originally had y < 50
+             *
+             * Note that we do want symmetry(0), lower/upper -> hence use y
+             */
+            else if (lower && -37.5193 < x  &&  x < 8.2924) {
+                /* Evaluate pnorm for x in (-37.5, -5.657) union (5.657, 37.5) */
+                xsq = 1.0 / (x * x); /* (1./x)*(1./x) might be better */
+                xnum = p[5] * xsq;
+                xden = xsq;
+                for (i = 0; i < 4; ++i) {
+                    xnum = (xnum + p[i]) * xsq;
+                    xden = (xden + q[i]) * xsq;
+                }
+                temp = xsq * (xnum + p[4]) / (xden + q[4]);
+                temp = (M_1_SQRT_2PI - temp) / y;
+
+                do_del(x);
+                swap_tail();
+            } else { /* large x such that probs are 0 or 1 */
+                if(x > 0) { cum = 1; }
+                else { cum = 0; }
+            }
+
+            return cum;
+        };
+    })();
+
+
+
+
+
+
+
+
 
     // http://home.online.no/~pjacklam/notes/invnorm/
     var probit = (function () {
